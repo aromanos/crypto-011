5326e731801fddcbc16d033ab3b42f10ae24c9d8
aromanos <andres@andresromanos.com>
2014-07-21 23:49:06 +0200

    Added Main class and so on

---
+++ week1/src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.andresromanos.crypto011.week1.Main;
+import org.junit.Test;
+
+/**
+ * Created by andres on 21/07/14.
+ */
+public class MainTest {
+
+    @Test
+    public void exec() {
+        new Main().exec();
+    }
+}
