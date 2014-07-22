11addabf0e8408e629337cbc9415d284c27669b4
Andres Romanos <andres@andresromanos.com>
2014-07-22 19:38:12 +0200

    Having fun...

--- week1/src/main/java/org/andresromanos/crypto011/week1/Main.java
+++ week1/src/main/java/org/andresromanos/crypto011/week1/Main.java
@@ -1,12 +1,10 @@
 package org.andresromanos.crypto011.week1;
 
-import static org.bouncycastle.pqc.math.linearalgebra.ByteUtils.xor;
-import org.bouncycastle.util.encoders.Hex;
+import static org.bouncycastle.pqc.math.linearalgebra.ByteUtils.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,16 +15,19 @@ public class Main {
 
     private List<String> cs = new ArrayList<>();
 
+    private static final String SPACE = " ";
+
     private Logger log = LoggerFactory.getLogger(Main.class);
 
     /**
      * Check http://crypto.stackexchange.com/questions/6020/many-time-pad-attack
       */
 
-    public void exec() {
+    public void exec() throws Exception {
+
+        String c = "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e";
 
         // cyphertext
-        cs.add("315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e");
         cs.add("234c02ecbbfbafa3ed18510abd11fa724fcda2018a1a8342cf064bbde548b12b07df44ba7191d9606ef4081ffde5ad46a5069d9f7f543bedb9c861bf29c7e205132eda9382b0bc2c5c4b45f919cf3a9f1cb74151f6d551f4480c82b2cb24cc5b028aa76eb7b4ab24171ab3cdadb8356f");
         cs.add("32510ba9a7b2bba9b8005d43a304b5714cc0bb0c8a34884dd91304b8ad40b62b07df44ba6e9d8a2368e51d04e0e7b207b70b9b8261112bacb6c866a232dfe257527dc29398f5f3251a0d47e503c66e935de81230b59b7afb5f41afa8d661cb");
         cs.add("32510ba9aab2a8a4fd06414fb517b5605cc0aa0dc91a8908c2064ba8ad5ea06a029056f47a8ad3306ef5021eafe1ac01a81197847a5c68a1b78769a37bc8f4575432c198ccb4ef63590256e305cd3a9544ee4160ead45aef520489e7da7d835402bca670bda8eb775200b8dabbba246b130f040d8ec6447e2c767f3d30ed81ea2e4c1404e1315a1010e7229be6636aaa");
@@ -39,13 +40,32 @@ public class Main {
         // target cyphertext
         //cs.add("32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904");
 
-        String result = null;
-        for (int i = 1; i < cs.size(); i++) {
-            if (result == null) {
-                result = cs.get(i - 1);
+        for (String csn : cs) {
+            //log.info("c1: {}", c1);
+            //log.info("csn: {}", csn);
+            byte[] result = strxor(c, csn);
+            log.info("xor: {}", toHexString(result));
+
+            for (byte by : result) {
+                byte[] arr = new byte[1];
+                arr[0] = by;
+                String ch = new String(arr, "UTF8");
+                //log.info("ch: {}", ch);
+                if (Character.isLowerCase(ch.charAt(0))) {
+                    log.info("Null found!");
+              }
             }
-            result = strxor(result, cs.get(i));
-            log.info("xor: {}", result);
+/*
+            for (int i = 0; i < result.length; i+=2) {
+                String str = Hex.toHexString(result).substring(i, i + 2);
+                log.info("str: {}", str);
+                String decoded = new String(Hex.decode(str));
+                log.info("decoded: {}", decoded);
+                if (str.equals("20")) {
+                    log.info("Space found!");
+                }
+            }
+            */
         }
     }
 
@@ -55,17 +75,17 @@ public class Main {
      * @param b the second string
      * @return XOR result
      */
-    private String strxor(String a, String b) {
-        byte[] b1 = Hex.decode(a);
-        byte[] b2 = Hex.decode(b);
+    private byte[] strxor(String a, String b) {
 
-        if (b1.length > b2.length) {
-            b1 = Arrays.copyOf(b1, b2.length);
+        if (a.length() > b.length()) {
+            a = a.substring(0, b.length());
         } else {
-            b2 = Arrays.copyOf(b2, b1.length);
+            b = b.substring(0, a.length());
         }
 
-        byte[] b3 = xor(b1, b2);
-        return Hex.toHexString(b3);
+        byte[] b1 = fromHexString(a);
+        byte[] b2 = fromHexString(b);
+
+        return xor(b1, b2);
     }
 }
--- week1/src/test/java/MainTest.java
+++ week1/src/test/java/MainTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class MainTest {
 
     @Test
-    public void exec() {
+    public void exec() throws Exception {
         new Main().exec();
     }
 }
